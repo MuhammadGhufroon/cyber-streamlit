1b2db74c99c6e32ca2588328dd1825ffcf6fd547
Muhammad Ghufroon <95112682+MuhammadGhufroon@users.noreply.github.com>
2022-07-23 06:35:21 +0700

    Add files via upload

--- main_page.py
+++ main_page.py
@@ -1,4 +1,17 @@
 import streamlit as st
+from multipage import MultiPage
+from pages import page_1, page_2, page_3
 
-st.markdown("# Main page 🎈")
-st.sidebar.markdown("# Main page 🎈")
+main_page = MultiPage()
+st.markdown('sjseijf')
+
+#menambahkan semua halaman di sini
+main_page.add_web("page_1", page_1.main_page)
+main_page.add_web("page_2", page_2.main_page)
+main_page.add_web("page_3", page_3.main_page)
+# Main Page
+main_page.run()
+
+st.title("Home Page")
+
+
